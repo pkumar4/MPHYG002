870cf6653ae3bf9cf6b06c5bbe0f3c740c116247
pkumar4 <prashant.kumar2@outlook.com>
2017-04-11 09:31:34 +0100

    Adding build folder

---
+++ build/test/CMakeFiles/TestConvexHull.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hpc/convexhull/test/grahamscan.cpp" "/home/hpc/convexhull/build/test/CMakeFiles/TestConvexHull.dir/grahamscan.o"
+  "/home/hpc/convexhull/test/jarvismarch.cpp" "/home/hpc/convexhull/build/test/CMakeFiles/TestConvexHull.dir/jarvismarch.o"
+  "/home/hpc/convexhull/test/test_convexhull.cpp" "/home/hpc/convexhull/build/test/CMakeFiles/TestConvexHull.dir/test_convexhull.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
